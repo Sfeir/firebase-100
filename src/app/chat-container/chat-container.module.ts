943fdcd54df8c22980987f9c72a026372683aab5
Wassim CHEGHAM <github@wassimchegham.com>
2017-12-11 14:04:38 +0100

    chore: refactor imports

--- src/app/chat-container/chat-container.module.ts
+++ src/app/chat-container/chat-container.module.ts
@@ -11,12 +11,12 @@ import { ChatItemComponent } from './chat-item/chat-item.component';
 import { ChatInputComponent } from './chat-input/chat-input.component';
 import { LoginComponent } from './login/login.component';
 import { LoginGuardService } from './login/login-guard.service';
+import { ROUTES } from './chat-container.routes';
+import { MatModule } from './../shared/material.module';
+
 import { StorageService } from './_shared/storage.service';
 import { DatabaseService } from './_shared/database.service';
 import { MessagingService } from './_shared/messaging.service';
-import { ROUTES } from './chat-container.routes';
-
-import { MatModule } from './../shared/material.module';
 import { AuthService } from './_shared/auth.service';
 
 @NgModule({
